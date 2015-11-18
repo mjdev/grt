a720cf5ec89b89b4a95dd3f2b98f27a87a9c3ed9
jahnen <jahnen@in.tum.de>
2015-11-18 15:24:38 +0100

    Add xcode framework project for apple watch

---
+++ iOS/grt.h
@@ -0,0 +1,19 @@
+//
+//  grt.h
+//  grt
+//
+//  Created by M J on 18/11/15.
+//  Copyright © 2015 jahnen. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for grt.
+FOUNDATION_EXPORT double grtVersionNumber;
+
+//! Project version string for grt.
+FOUNDATION_EXPORT const unsigned char grtVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <grt/PublicHeader.h>
+
+
